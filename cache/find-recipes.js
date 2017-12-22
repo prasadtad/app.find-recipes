70307737379bfb7b6f11aab275609483abffff2f
Sri Prasad Tadimalla <prasad_tad@yahoo.com>
2017-12-22 20:29:09 +1100

    using redis-poco now

--- cache/find-recipes.js
+++ cache/find-recipes.js
@@ -1,66 +1,43 @@
 // cache/find-recipes.js
 
-const RecipeKeys = require('./recipe-keys')
 const _ = require('lodash')
-const RedisProxy = require('../proxies/redis-proxy')
+const RedisPoco = require('redis-poco')
 const RedisPhraseComplete = require('redis-phrase-complete')
 
 module.exports = class FindRecipes
 {
     constructor()
     {        
-        this.redisProxyClient = new RedisProxy.Client()
-        this.keys = new RecipeKeys(this.redisProxyClient.seperator)
-        this.redisPhraseComplete = new RedisPhraseComplete({ client: this.redisProxyClient.client, namespace: this.keys.Autocomplete })
-        _.bindAll(this, 'buildKey', 'whenFilter', 'whenFind', 'whenQuit')
-    }
-    
-    buildKey(setPrefix, flag) 
-    {
-        return setPrefix + this.redisProxyClient.seperator + (flag ? 'True' : 'False')
+        this.redisPoco = new RedisPoco({ namespace: 'recipe', itemKey: 'item', endpoint: process.env.CACHE_ENDPOINT, attributes: [ 'vegan', 'totalTimeInMinutes', 'approved', 'spiceLevel', 'region', 'cuisine', 'chefId', 'ingredientIds', 'overnightPreparation', 'accompanimentIds', 'collections' ]})
+        this.redisPhraseComplete = new RedisPhraseComplete({ namespace: 'recipe:autocomplete', client: this.redisPoco.client })
+        _.bindAll(this, 'whenFilter', 'whenFind', 'whenQuit')
     }
 
     whenFilter(filter)
     {
-        const keyPromises = [];
-        if (filter.ingredientIds && filter.ingredientIds.length > 0)
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.IngredientId, filter.ingredientIds));
-        if (filter.regions && filter.regions.length > 0) 
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.Region, filter.regions));
-        if (filter.cuisines && filter.cuisines.length > 0) 
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.Cuisine, filter.cuisines));
-        if (filter.spiceLevels && filter.spiceLevels.length > 0) 
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.SpiceLevel, filter.spiceLevels));
-        if (filter.totalTimes && filter.totalTimes.length > 0) 
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.TotalTime, filter.totalTimes));
-        if (filter.collections && filter.collections.length > 0) 
-            keyPromises.push(this.redisProxyClient.whenSetOr(this.keys.Collection, filter.collections));
-        const keys = [];
-        if (filter.vegan) keys.push(this.buildKey(this.keys.Vegan, filter.vegan))
-        if (filter.overnightPreparation) keys.push(this.buildKey(this.keys.OvernightPreparation, filter.overnightPreparation))
-        if (keyPromises.length == 0 && keys.length == 0) return Promise.resolve([])
-        return Promise.all(keyPromises)
-                    .then(k => this.redisProxyClient.whenSetsAnd(_.union(k, keys)))
-                    .then(setKey => this.redisProxyClient.whenMembers(setKey))
+        filter.approved = true
+        return this.redisPoco.whenFilter(filter)
     }
 
     whenFind(searchPhrase) 
     {
         return this.redisPhraseComplete.whenFind(searchPhrase)
-                    .then(ids => Promise.all(_.map(ids, id => this.redisProxyClient.whenGet(this.keys.Names, id)))
-                                    .then(allNames => {
-                                        const results = []
-                                        for (let i=0; i<ids.length; i++)
-                                        {
-                                            const name = _.find(allNames[i].split('\n'), name => name.toLowerCase().includes(searchPhrase))
+                    .then(ids => Promise.all(_.map(ids, this.redisPoco.whenGet))
+                                .then(allRecipes => {
+                                    const results = []
+                                    for (let i=0; i<ids.length; i++)
+                                    {
+                                        if (allRecipes[i].approved) {
+                                            const name = _.find(allRecipes[i].names, name => name.toLowerCase().replace(/[(),!\\.-]+/g,' ').includes(searchPhrase))
                                             results.push({name: name, id: ids[i]})
                                         }
-                                        return Promise.resolve(results)
-                                    }))                    
+                                    }
+                                    return Promise.resolve(results)
+                                }))                    
     }
 
     whenQuit()
     {
-        return this.redisProxyClient.whenQuit()
+        return this.redisPoco.whenQuit()
     }
 }
--- tests.js
+++ tests.js
@@ -12,17 +12,19 @@ const redis = require('redis-promisify')
 const index = require('./index')
 
 const autocompleteEvent = {
-    'name': 'spiced vegetables'
+    'name': 'peanut sesame'
 }
 
 const filterEvent = {
-    'ingredientIds': ['DfgIn-bx','N2YXf2ns','jnZf_ewk'],
-	'spiceLevels': ['Hot'],
-	'regions': ['Indian Subcontinent'],
+    'accompanimentIds': ['NXkkUWRu'],
+    'chefId': 'auth0|5710de6b3c7ae40538abdc35',
+    'ingredientIds': ['-6US1xKg','GW_XXZqX','_hfTfh3z'],
+	'spiceLevel': ['Hot'],
+	'region': ['Indian Subcontinent'],
 	'overnightPreparation': false,
-	'collections': ['Curries','Side dishes'],
-	'cuisines': ['South Indian'],
-    'totalTimes': ['Regular'],
+	'collections': ['Dinner','Rice dishes'],
+	'cuisine': ['South Indian'],
+    'totalTimeInMinutes': { min: 80, max: 100 },
     'vegan': false
 }
 
@@ -34,8 +36,17 @@ const whenLoadTestData = () => {
                     const trans = client.multi()
                     for (const key of _.keys(testData))
                     {
-                        if (Array.isArray(testData[key]))
-                            trans.sadd(key,testData[key])
+                        if (Array.isArray(testData[key])) {
+                            for (const value of testData[key])
+                            {
+                                if (_.isObject(value)) {
+                                    for (const id of _.keys(value))
+                                        trans.zadd(key, value[id], id)
+                                }
+                                else
+                                    trans.sadd(key, testData[key])
+                            }
+                        }
                         else
                         {
                             for (const hashField of _.keys(testData[key]))
@@ -60,13 +71,13 @@ tests.push(whenLoadTestData()
                 .then(() => index.whenHandler(filterEvent))
                 .then(results => {
                     testMessages.push('Filter recipes')
-                    assert.deepEqual(results, ['NXkkUWRu'])
+                    assert.deepEqual(results, ['3uDSc4Vg'])
                     return Promise.resolve()
                 })         
                 .then(() => index.whenHandler(autocompleteEvent))
                 .then(results => {
                     testMessages.push('Autocomplete recipe name')
-                    assert.deepEqual(results, [{name: 'Slow-Cooked Spiced Vegetables and Rice', id: '3uDSc4Vg'}])
+                    assert.deepEqual(results, [{name: 'Eggplant in a Peanut-Sesame Gravy', id: 'NXkkUWRu'}])
                     return Promise.resolve()
                 })
             )            
@@ -75,6 +86,10 @@ Promise.all(tests)
         .then(() => {
             console.info(_.map(testMessages, m => m + ' - passed').join('\n'))
             process.exit()
+        })
+        .catch(err => {
+            console.error(err)
+            process.exit()            
         })
 
 
