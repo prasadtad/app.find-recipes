8b3df4428487b921acd25628f1d6d1bb8e345ac7
Sri Prasad Tadimalla <prasad_tad@yahoo.com>
2018-04-01 22:11:52 +1000

    ingredient search bug fix

--- cache/find-recipes.js
+++ cache/find-recipes.js
@@ -1,6 +1,8 @@
 // cache/find-recipes.js
 
 const _ = require('lodash')
+require('lodash.product')
+
 const RedisPoco = require('redis-poco')
 const RedisPhraseComplete = require('redis-phrase-complete')
 
@@ -20,14 +22,19 @@ module.exports = class FindRecipes
         filter.approved = true
         if (!filter.ingredients)
             return this.redisPoco.whenFilter(filter)
-        return Promise.all(_.map(filter.ingredients.split(/,|and/g), ingredient => 
-            this.ingredientsPhraseComplete.whenFind(_.trim(ingredient))))
-                .then(allResults => {
-                    allResults = _.flatten(allResults)
-                    filter.ingredientIds = _.map(allResults, result => result.id)
-                return this.redisPoco.whenFilter(filter)                
-            })
-    }    
+        const whenFindIngredients = Promise.all(_.map(filter.ingredients.split(/,|and/g), ingredient => 
+                                                this.ingredientsPhraseComplete.whenFind(_.trim(ingredient))))
+        return whenFindIngredients
+            .then(allResults => {
+                allResults = _.product(..._.map(allResults, results => _.uniq(_.map(results, result => result.id))))
+                
+                return Promise.all(_.map(allResults, allResult => {
+                    const filterWithIngredientIds = _.cloneDeep(filter)
+                    filterWithIngredientIds.ingredientIds = allResult
+                    return this.redisPoco.whenFilter(filterWithIngredientIds)
+                })).then(results => Promise.resolve(_.concat(...results)))                
+        })
+    }
 
     whenFind(searchPhrase) 
     {
